6f500127c2b0067856bcfc874449fa6cf4b4197e
lilaluca <ml.kochsiek@mailbox.org>
2017-11-03 15:18:21 +0100

    Adds static_pages controller and converts app/view/*.html.erb into *.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+
+  get 'static_pages/calendar'
+
+  get 'static_pages/blog'
+
+  get 'static_pages/press'
+
+  get 'static_pages/participate'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/imprint'
+
+  root 'static_pages#home'
 end
