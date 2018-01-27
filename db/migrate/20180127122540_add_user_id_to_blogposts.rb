94fbe4230e962e054bafe5437f291e0d8ac8572c
catcosmo <astrangethought@hotmail.com>
2018-01-27 19:38:16 +0100

    deleted double migrations and re-created db

--- db/migrate/20180127122540_add_user_id_to_blogposts.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToBlogposts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :blogposts, :user_id, :int
-  end
-end
--- db/migrate/20180127134652_fix_column_name_author_name.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnNameAuthorName < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :blogposts, :author_name, :display_name
-  end
-end
