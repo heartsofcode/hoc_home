f21df059f03f7ea4943e29432d6f2d7b7747019c
katze <catcosmo@users.noreply.github.com>
2018-01-31 13:01:25 +0100

    fix stupid migration typo - alles wird gut!

--- db/migrate/20180127133805_fix_column_name_author_name.rb
+++ db/migrate/20180127133805_fix_column_name_author_name.rb
@@ -1,5 +1,5 @@
 class FixColumnNameAuthorName < ActiveRecord::Migration[5.1]
   def change
-    rename_column :blogpostsrail, :author_name, :display_name
+    rename_column :blogposts, :author_name, :display_name
   end
 end
