3f47b0c372c889727991e506673ccac5c53e7592
lilaluca <ml.kochsiek@mailbox.org>
2018-03-20 12:18:47 +0100

    Fixing static pages test <3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,43 +1,38 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get home safe and dry" do
+    get root_url
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-  end
-
-  test "should get calendar" do
-    get static_pages_calendar_url
+    get about_url
     assert_response :success
   end
 
   test "should get blog" do
-    get static_pages_blog_url
+    get blog_url
     assert_response :success
   end
 
   test "should get press" do
-    get static_pages_press_url
+    get press_url
     assert_response :success
   end
 
   test "should get participate" do
-    get static_pages_participate_url
+    get participate_url
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
   end
 
   test "should get imprint" do
-    get static_pages_imprint_url
+    get imprint_url
     assert_response :success
   end
 
