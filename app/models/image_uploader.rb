e4bb47f804cf0b6aaf47cb0cf81c18409ba371c3
lilaluca <ml.kochsiek@mailbox.org>
2018-03-26 00:42:05 +0200

    Ich glaube ich hab's:
    * Includes ImageUploader in Blogpost model
    * Additionally validates presence of image when creating blogposts
    * Fix naming error during image resizing process

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -29,13 +29,12 @@ class ImageUploader < Shrine
   end
 
   def process(io, context)
-    if context[:phase] == :store
-      size_700 = resize_to_limit!(io.download, 700, 700)
-      size_500 = resize_to_limit(size_700,    500, 500)
-      size_300 = resize_to_limit(size_500,    300, 300)
-      thumb = resize_to_limit(size_300, 200, 200)
+    return unless context[:phase] == :store
+    size_700 = resize_to_limit!(io.download, 700, 700)
+    size_500 = resize_to_limit(size_700, 500, 500)
+    size_300 = resize_to_limit(size_500, 300, 300)
+    thumb = resize_to_limit(size_300, 200, 200)
 
-      { original: io, large: size_700, medium: size_500, small: size_300, thumb: thumb }
-    end
+    { original: io, large: size_700, medium: size_500, small: size_300, thumb: thumb }
   end
 end
