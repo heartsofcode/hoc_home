e9dc62e5f367d88f9d2ab5163459eaf1903988c1
lilaluca <ml.kochsiek@mailbox.org>
2018-03-29 22:19:25 +0200

    Rerouting for kontakt, presse, impressum

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   get 'about',              to: 'static_pages#about'
   get 'mitmischen',         to: 'static_pages#mitmischen'
   get 'advent',             to: 'static_pages#advent'
-  get 'contact',            to: 'static_pages#contact'
+  get 'kontakt',            to: 'static_pages#kontakt'
   get 'nussletter',         to: 'static_pages#newsletter'
   get 'satzung',            to: 'static_pages#satzung'
   get 'mitgliedsformular',  to: 'static_pages#mitgliedsformular'
-  get 'imprint',            to: 'static_pages#imprint'
-  get 'press',              to: 'static_pages#press'
+  get 'impressum',          to: 'static_pages#impressum'
+  get 'presse',             to: 'static_pages#presse'
 
   root 'static_pages#home'
 end
