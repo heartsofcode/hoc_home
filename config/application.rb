5b1cce7c02b942860c0c78652690716a3cc729e5
lilaluca <ml.kochsiek@mailbox.org>
2018-03-31 23:51:17 +0200

    This way the calendar and events just look a bit better, huh?!

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module HocHome
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.beginning_of_week = :monday
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
