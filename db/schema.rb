0c7fa84ae33b51d3af85927c74e869c7c147ccbd
lilaluca <ml.kochsiek@mailbox.org>
2018-04-01 12:10:06 +0200

    Adding description to events for the curious ones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331211837) do
+ActiveRecord::Schema.define(version: 20180331222834) do
 
   create_table "blogposts", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20180331211837) do
     t.datetime "updated_at", null: false
     t.integer "calendar_id"
     t.string "location_url"
+    t.text "description"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
