6e914b7451796f0cf4f2e8f9876bf35e57abdf85
lilaluca <ml.kochsiek@mailbox.org>
2018-04-06 23:34:32 +0200

    Improving events view on formatting time & some minor nicefications

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,17 +1,10 @@
 module EventsHelper
 
-  def nicify_start_date(event)
-    event.start_time.strftime("%d %b %Y")
+  def prettify(event)
+    @formats = []
+    @formats << event.start_time.strftime("%d %b %Y")
+    @formats << event.start_time.strftime("%R")
+    @formats << event.end_time.strftime("%d %b %Y")
+    @formats << event.end_time.strftime("%R")
   end
-  def nicify_start_time(event)
-    event.start_time.strftime("%H:%M")
-  end
-
-  def nicify_end_date(event)
-    event.end_time.strftime("%d %b %Y")
-  end
-  def nicify_end_time(event)
-    event.end_time.strftime("%H:%M")
-  end
-  
 end
